d32587c77639acf244ceff03dd6357fa0fe924a4
Samuel Murugasu <samuelmurugasu@gmail.com>
2024-04-22 01:58:01 +0800

    Fix iron

--- bakery/iron.py
+++ bakery/iron.py
@@ -1,18 +1,16 @@
 from .sieve import *
+from .law_school import Painter
 
 def check_file(L, filename):
-    with open(filename) as file:
-        full_str = file.read()
-        words = tuple(w.upper() for w in full_str.split('\n'))
+	with open(filename) as file:
+		full_str = file.read()
+		words = tuple(w.upper() for w in full_str.split('\n'))
 
-    for word in words:
-        if len(word) != L:
-            raise Exception(f"Word '{a}' in {filename} does not match length {L}.")
+	for word in words:
+		if len(word) != L:
+			raise Exception(f"Word '{a}' in {filename} does not match length {L}.")
 
-    return words
-
-FIVES = "../fridge/possible_words.txt"
-SEVENS = ""
+	return words
 
 class Iron:
 	def __init__(self, size, tiles, colours):
@@ -62,21 +60,18 @@ class Iron:
 	
 	# rows and columns -> big string
 	def reduce(self, splits):
-		lst = [None for _ in range(self.size ** 2 - (self.size // 2) ** 2)]
+		lst = ['_' for _ in range(self.size ** 2 - (self.size // 2) ** 2)]
 		for j in range(len(self.indss)):
 			splist = splits[j]
 			inds = self.indss[j]
 			for i in range(len(inds)):
-				lst[inds[i]] = splist[i]
+				if splist[i].isalpha():
+					lst[inds[i]] = splist[i]
 		
 		return ''.join(lst)
 
-	def redox(self, splits):
-		return self.oxidize(self.reduce(splits))
-
 	def rust(self, guesses, word_pools):
 		not_guessed = []
-		new_pools = []
 		for i in range(len(guesses)):
 			if guesses[i].isalpha():
 				# already guessed
@@ -86,44 +81,65 @@ class Iron:
 				# prune this branch
 				return
 			not_guessed.append(i)
-		
+
 		if not_guessed == []:
 			# successfully completed guess
-			self.sols.append(tuple(guess))
+			self.sols.append(tuple(guesses))
 			return
 		
 		# to minimize iterations
-		best_i = min(not_guessed, lambda i: len(word_pools[i]))
+		best_i = min(not_guessed, key = lambda i: len(word_pools[i]))
 		
 		for guess in word_pools[best_i]:
-			new_guesses = guesses.copy()
-			new_guesses[best_i] = match
+			new_guesses = list(guesses)
+			new_guesses[best_i] = guess
 			
 			FeO = self.reduce(new_guesses)
 			Fe2O3 = self.oxidize(FeO)
 			
-			new_letters = get_letter_counts(FeO)
+			used_letters = get_letter_counts(FeO)
+			new_letters = subtract_letter_counts(self.letter_pool, used_letters)
 			new_pools = [() for _ in word_pools]
 			for i in not_guessed:
 				if i == best_i:
 					continue
 				new_pools[i] = filter_by_shadow(word_pools[i], Fe2O3[i], new_letters)
 			
-			self.rust(new_guesses, new_pools)
+			self.rust(Fe2O3, new_pools)
 
 		return self.sols
 
 	def solve(self, word_pool):
-		# need to (guess -> update pool AND colouring -> refilter -> guess -> ...)
+		# need to (guess -> update pool -> refilter -> guess -> ...)
 		
 		new_pool = filter_by_letters(word_pool, self.letter_pool)
 		
 		lets = self.oxidize(self.tiles)
 		cols = self.oxidize(self.colours)
-		word_pools = [filter_by_colouring(new_pool, lets[i], cols[i]) for i in range(self.size + 1)]
-		
+
+		# this filter is incorrect because intersection letters may apply to either word
+		#word_pools = [filter_by_colouring(new_pool, lets[i], cols[i]) for i in range(self.size + 1)]
+		word_pools = [filter_by_green(new_pool, lets[i], cols[i]) for i in range(self.size + 1)]
+
 		blank_guesses = ['_' * self.size for i in range(self.size + 1)]
 		
 		self.rust(blank_guesses, word_pools)
 		
+		return self.sols
+
+	# we don't rigorously use the colouring, so we might as well check
+	def verify_solutions(self):
+		lets = self.oxidize(self.tiles)
+
+		new_sols = []
+		for sol in self.sols:
+			sol_cols = [Painter.colour(lets[i], sol[i]) for i in range(self.size + 1)]
+			
+			# need to turn R into _ to account for overlap
+			sol_cols = list(map(R_to_U, sol_cols))
+			sol_colours = U_to_R(self.reduce(sol_cols))
+			
+			if sol_colours == self.colours:
+				new_sols.append(sol)
+		self.sols = new_sols
 		return self.sols
