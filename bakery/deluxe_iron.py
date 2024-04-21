55b9860c10f41b018be7ffc6524c770226d32f71
Samuel Murugasu <samuelmurugasu@gmail.com>
2024-04-22 02:19:08 +0800

    Make Deluxe and Waffle versions of Iron

---
+++ bakery/deluxe_iron.py
@@ -0,0 +1,20 @@
+from .iron import Iron
+
+class DeluxeIron(Iron):
+    # 00 01 02 03 04 05 06
+    # 07    08    09    10
+    # 11 12 13 14 15 16 17
+    # 18    19    20    21
+    # 22 23 24 25 26 27 28
+    # 29    30    31    32
+    # 33 34 35 36 37 38 39
+        
+    SIZE = 7
+    FILE = None
+    
+    def __init__(self, tiles, colours):
+        super().__init__(DeluxeIron.SIZE, tiles, colours)
+
+    def solve(self):
+        super().solve(check_file(DeluxeIron.SIZE, DeluxeIron.FILE))
+
---
+++ bakery/waffle_iron.py
@@ -0,0 +1,17 @@
+from .iron import Iron
+
+class WaffleIron(Iron):
+    # 00 01 02 03 04
+    # 05    06    07
+    # 08 09 10 11 12
+    # 13    14    15
+    # 16 17 18 19 20
+
+    SIZE = 5
+    FILE = "../fridge/possible_words.txt"
+
+    def __init__(self, tiles, colours):
+        super().__init__(WaffleIron.SIZE, tiles, colours)
+
+    def solve(self):
+        super().solve(check_file(WaffleIron.SIZE, WaffleIron.FILE))
