2e6f6cb3fffdfde45f8a442e9eceb93c74227a66
Samuel Murugasu <samuelmurugasu@gmail.com>
2024-04-21 17:23:37 +0800

    Do up iron and sieve logic

--- bakery/iron.py
+++ bakery/iron.py
@@ -1,4 +1,4 @@
-#from .law_school import Painter
+from .sieve import *
 
 def check_file(L, filename):
     with open(filename) as file:
@@ -11,52 +11,119 @@ def check_file(L, filename):
 
     return words
 
-FIVES = "./fridge/possible_words.txt"
+FIVES = "../fridge/possible_words.txt"
 SEVENS = ""
 
-def get_matches(g, col, words):
-	if not len(g) == len(col):
-		raise Exception(f"String '{g}' and Colouring '{col}' not of same length.")
-	
-	l = len(g)
-	
-	matches = []
-	for word in words:
-		if len(word) != l:
-			raise Exception(f"Word {word} in checking set not of length {l}.")
-		if Painter.is_match(g, word, col):
-			matches.append(word)
-	
-	return tuple(matches)
+class Iron:
+	def __init__(self, size, tiles, colours):
+		# skipping validity checks
+		
+		self.size = size
+		self.tiles = tiles.upper()
+		self.colours = colours.upper()
+		self.letter_pool = get_letter_counts(self.tiles)
+		self.sols = []
+		
+		# creating indices reference
+		indss = []
+		
+		row_jump = size + size // 2 + 1
+		for r in range(size // 2 + 1):
+			row_start = r * row_jump
+			row_inds = tuple(range(row_start, row_start + size))
+			indss.append(row_inds)
 
-def split_up(size, strs):
-	num_tiles = size ** 2 - (size // 2) ** 2
-	if len(strs) != num_tiles:
-		raise Exception(f"Expected {num_tiles} tiles, got {len(strs)}.")
-	
-	words = []
-	
-	row_jump = size + size // 2 + 1
-	for i in range(size // 2 + 1):
-		row_start = i * row_jump
-		row_word = strs[row_start : row_start + size]
-		words.append(row_word)
-
-	column_e2o = size
-	column_o2e = size // 2 + 1
-	for i in range(size // 2 + 1):
-		column_i = i * 2
-		column_word = strs[column_i]
-		for j in range(size // 2):
-			column_i += column_e2o
-			column_word += strs[column_i]
+		column_e2o = size
+		column_o2e = size // 2 + 1
+		for c in range(size // 2 + 1):
+			curr_i = c * 2
+			column_inds = [curr_i]
+			for _ in range(size // 2):
+				curr_i += column_e2o
+				column_inds.append(curr_i)
+				
+				curr_i += column_o2e
+				column_inds.append(curr_i)
 			
-			column_i += column_o2e
-			column_word += strs[column_i]
+			indss.append(tuple(column_inds))
+			column_e2o -= 1
+			column_o2e += 1
 		
-		words.append(column_word)
+		self.indss = tuple(indss)
 		
-		column_e2o -= 1
-		column_o2e += 1
+	# big string -> rows (top to bottom) and columns (left to right)
+	def oxidize(self, strs):
+		splits = []
+		for inds in self.indss:
+			splist = [strs[i] for i in inds]
+			splits.append(''.join(splist))
+		
+		return tuple(splits)
 	
-	return words
+	# rows and columns -> big string
+	def reduce(self, splits):
+		lst = [None for _ in range(self.size ** 2 - (size // 2) ** 2)]
+		for j in range(len(self.indss)):
+			splist = splits[j]
+			inds = self.indss[j]
+			for i in range(len(inds)):
+				lst[inds[i]] = splist[i]
+		
+		return ''.join(lst)
+
+	def redox(self, splits):
+		return self.oxidize(self.reduce(splits))
+
+	def rust(self, guesses, word_pools):
+		not_guessed = []
+		new_pools = []
+		for i in range(len(guesses)):
+			if guesses[i].isalpha():
+				# already guessed
+				continue
+			if word_pools[i] == ():
+				# not yet guessed and no candidates
+				# prune this branch
+				return
+			not_guessed.append(i)
+		
+		if not_guessed == []:
+			# successfully completed guess
+			self.sols.append(tuple(guess))
+			return
+		
+		# to minimize iterations
+		best_i = min(not_guessed, lambda i: len(word_pools[i]))
+		
+		for guess in word_pools[best_i]:
+			new_guesses = guesses.copy()
+			new_guesses[best_i] = match
+			
+			FeO = self.reduce(new_guesses)
+			Fe2O3 = self.oxidize(FeO)
+			
+			new_letters = get_letter_counts(FeO)
+			new_pools = [() for _ in word_pools]
+			for i in not_guessed:
+				if i == best_i:
+					continue
+				new_pools[i] = filter_by_shadow(word_pools[i], Fe2O3[i], new_letters)
+			
+			self.rust(new_guesses, new_pools)
+
+		return self.sols
+
+	def solve(self, word_pool):
+		# need to (guess -> update pool AND colouring -> refilter -> guess -> ...)
+		
+		new_pool = filter_by_letters(word_pool, self.letter_pool)
+		
+		lets = self.oxidize(self.tiles)
+		cols = self.oxidize(self.colours)
+		word_pools = [filter_by_colouring(new_pool, lets[i], cols[i]) for i in range(self.size + 1)]
+		
+		blank_guesses = ['_' * self.size for i in range(size + 1)]
+		
+		self.rust(blank_guesses, word_pools)
+		
+		return self.sols
---
+++ bakery/sieve.py
@@ -0,0 +1,64 @@
+from .law_school import Painter
+
+# Pool Filters
+
+def filter_by_colouring(word_cool, g, col):
+	return tuple(filter(lambda word: Painter.is_match(g, word, col), word_pool))
+
+def get_letter_counts(word):
+	counts = {}
+	for letter in word:
+		if not letter.isalpha():
+			continue
+		let = letter.upper()
+		if let not in counts:
+			counts[let] = 0
+		counts[let] += 1
+	return counts
+
+# dict of "aabcd" - "ad" -> "abc"
+# must have bet <= alpha (subset)
+def subtract_letter_counts(alpha, beta):
+	gamma = alpha.copy()
+	for b in beta:
+		gamma[b] -= beta[b]
+	return gamma
+
+def fits_in_pool(word, letter_pool):
+	counts = get_letter_counts(word)
+	for letter in counts:
+		if letter not in letter_pool:
+			return False
+		if counts[letter] > letter_pool[letter]:
+			return False
+	else:
+		return True
+
+def filter_by_letters(word_pool, letter_pool):
+	output = []
+	return tuple(filter(lambda word: fits_in_pool(word, letter_pool), word_pool))
+
+# "_" for blanks
+def filter_by_template(word_pool, template):
+	tem = template.upper()
+	alphas = [i for i in range(len(tem)) if tem[i].isalpha()]
+	output = []
+	for word in word_pool:
+		for i in alphas:
+			if word[i].upper() != tem[i]:
+				break
+		else:
+			output.append(word)
+	return tuple(output)
+
+# 'T____' with {T : 0, ...} will accept 'TAXES' but not 'TITAN'
+def filter_by_shadow(word_pool, template, letter_pool):
+	new_pool = filter_by_template(word_pool, template)
+	
+	space = [i for i in range(len(tem)) if not template[i].isalpha()]
+	output = []
+	for word in new_word:
+		cut_word = ''.join(word[i] for i in space)
+		if fits_in_pool(cut_word, letter_pool):
+			output.append(word)
+	return tuple(output)
